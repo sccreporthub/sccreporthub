f60a9e77f1aa59aed08ecb2404cd7a0ef8d4d030
SCC ReportHub <sccreporthub@gmail.com>
2026-08-09 21:03:16 +0800

    Update UserSeeder with actual maintenance staff

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -48,24 +48,27 @@ class UserSeeder extends Seeder
         ]);
 
         // Maintenance Staff
-        User::firstOrCreate(['email' => 'maintenance@scc.edu.ph'], [
-            'role_id'        => $maintenanceRole->id,
-            'first_name'     => 'Pedro',
-            'last_name'      => 'Reyes',
-            'password'       => Hash::make('Maintenance@1234'),
-            'department'     => 'Facilities & Maintenance',
-            'contact_number' => '09000000004',
-            'status'         => 'active',
-        ]);
+        $maintenanceUsers = [
+            ['email' => 'rcagay@scc.edu.ph',    'first_name' => 'Ricardo',    'last_name' => 'Cagay',    'contact_number' => '09000000010'],
+            ['email' => 'rsucayre@scc.edu.ph',  'first_name' => 'Richard',    'last_name' => 'Sucayre',  'contact_number' => '09000000011'],
+            ['email' => 'jselma@scc.edu.ph',    'first_name' => 'Janry',      'last_name' => 'Selma',    'contact_number' => '09000000012'],
+            ['email' => 'eomotong@scc.edu.ph',  'first_name' => 'Edgar',      'last_name' => 'Omotong',  'contact_number' => '09000000013'],
+            ['email' => 'mcaingcoy@scc.edu.ph', 'first_name' => 'Mike Raven', 'last_name' => 'Caingcoy', 'contact_number' => '09000000014'],
+            ['email' => 'egudoy@scc.edu.ph',    'first_name' => 'Erwin',      'last_name' => 'Gudoy',    'contact_number' => '09000000015'],
+            ['email' => 'winbing@scc.edu.ph',   'first_name' => 'Winsor',     'last_name' => 'Inbing',   'contact_number' => '09000000016'],
+            ['email' => 'vbonzo@scc.edu.ph',    'first_name' => 'Vicente',    'last_name' => 'Bonzo',    'contact_number' => '09000000017'],
+        ];
 
-        User::firstOrCreate(['email' => 'maintenance2@scc.edu.ph'], [
-            'role_id'        => $maintenanceRole->id,
-            'first_name'     => 'Carlos',
-            'last_name'      => 'Mendoza',
-            'password'       => Hash::make('Maintenance@1234'),
-            'department'     => 'Facilities & Maintenance',
-            'contact_number' => '09000000005',
-            'status'         => 'active',
-        ]);
+        foreach ($maintenanceUsers as $user) {
+            User::firstOrCreate(['email' => $user['email']], [
+                'role_id'        => $maintenanceRole->id,
+                'first_name'     => $user['first_name'],
+                'last_name'      => $user['last_name'],
+                'password'       => Hash::make('Maintenance@1234'),
+                'department'     => 'Facilities & Maintenance',
+                'contact_number' => $user['contact_number'],
+                'status'         => 'active',
+            ]);
+        }
     }
 }
